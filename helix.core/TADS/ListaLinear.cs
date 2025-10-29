c76828b74d3dfd379ecc44ec94cec90f13deb2fa
Renzzo Rocha <renzzorocha17@gmail.com>
2025-10-29 19:11:46 -0300

    feat: add lista linear TAD

---
+++ helix.core/TADS/ListaLinear.cs
@@ -0,0 +1,19 @@
+﻿namespace helix.core.TADS;
+
+public class ListaLinear
+{
+    private int[] elementos;
+    private int inicio;
+    private int fim;
+    private int capacidade;
+
+    public ListaLinear(int tamanho)
+    {
+        capacidade = tamanho;
+        elementos = new int[capacidade];
+        inicio = 0;
+        fim = 0;
+    }
+    
+    
+}
